d7ec353323fca01d5fa23038dd7dac8599a69e4c
Zardinality <asxaqz2@gmail.com>
2018-12-12 01:05:28 +0800

    change the torch version back, repair the space in command parameter for cuda_output

--- research.py
+++ research.py
@@ -43,10 +43,12 @@ import pytreebank
 from os.path import exists
 from wheel.pep425tags import get_abbr_impl, get_impl_ver, get_abi_tag
 platform = '{}{}-{}'.format(get_abbr_impl(), get_impl_ver(), get_abi_tag())
-cuda_output = !ldconfig - p | grep cudart.so | sed - e 's/.*\.\([0-9]*\)\.\([0-9]*\)$/cu\1\2/'
+cuda_output = !ldconfig -p | grep cudart.so | sed -e 's/.*\.\([0-9]*\)\.\([0-9]*\)$/cu\1\2/'
 accelerator = cuda_output[0] if exists('/dev/nvidia0') else 'cpu'
-
-!pip install -q http://download.pytorch.org/whl/{accelerator}/torch-1.0.0-{platform}-linux_x86_64.whl torchvision
+!pip install -q http://download.pytorch.org/whl/{accelerator}/torch-0.4.1-{platform}-linux_x86_64.whl torchvision
+# if accelerator == 'cpu':
+#   raise InvalidArgumentError('should run this notebook under gpu enviroment')
+# !pip install torch torchvision
 
 # %%
 from google.colab import drive
@@ -213,3 +215,5 @@ for rs_s in rd_s_li:
     exp_li.append(list(do_experiment(rs_s)))
 
 # %%
+
+!cp ./*.pt "/gdrive/My Drive/pts"
